36cc4730593a1cc5253fea60485c600073a38b51
chris <chris@republicbjj.com>
2020-04-18 14:41:34 -0600

    added code to server.js and a quick post to app.js

--- website/app.js
+++ website/app.js
@@ -3,3 +3,29 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+///////
+const postData = async ( url = '', data = {})=>{
+    console.log(typeof JSON.stringify(data));
+    const response = await fetch(url, {
+        method: 'POST', 
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // Body data type must match "Content-Type" header        
+        body: JSON.stringify(data), 
+    });
+    console.log(response);
+
+    try {
+        const newData = await response.json();
+        console.log('try ' + newData);
+        return newData;
+    }catch(error) {
+        console.log('error ', error);
+    }
+}
+
+postData('/postWeather', {zip: 't2v 3w4', feelings: 'sad'});
